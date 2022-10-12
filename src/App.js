dc0dd1342eaedd18c648837e7a17e83fd88a51b1
raselASrock <mdraselm413@gmail.com>
2022-10-13 00:35:07 +0600

    toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import './App.css';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
 function App() {
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () =>{
     console.log('google-comming-soon');
     signInWithPopup(auth, provider)
     .then(result => {
       const user = result.user;
+      setUser(user)
       console.log(user);
     })
     .catch( error => {
       console.error('error:', error);
     })
   }
+
+  const handleSignOut = () =>{
+    console.log('SignOuted');
+    signOut(auth)
+    .then( () => {
+      setUser({})
+    }) 
+    .catch( () => {
+      setUser({})
+    })
+  }
   return (
     <div className="App">
+      { user.email ?
+        <button onClick={handleSignOut}>Sign Out</button>
+        :
       <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      }
+      {user.email && <div>
+        <h3>User Name: {user.displayName}</h3>
+        <p>User Email: {user.email}</p>
+        <img src={user.photoURL} alt="" />
+      </div>}
     </div>
   );
 }
