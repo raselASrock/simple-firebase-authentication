6ca1fc9f394401020015a586804c337388c1af7f
raselASrock <mdraselm413@gmail.com>
2022-10-12 21:28:57 +0600

    data

---
+++ _steps.js
@@ -0,0 +1,9 @@
+/* 
+1. visit: https://console.firebase.google.com/u/0/
+2. create a new firebase project
+3. visit docs
+4. register web app
+5. install firebase for our project
+6. Danger Step: firebase config
+7. export app from firebase congig
+*/
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACefzcjTx0oc3kQBT2wRGcHUgHNGjeJcM",
+  authDomain: "simple-firebase-authenti-ddc20.firebaseapp.com",
+  projectId: "simple-firebase-authenti-ddc20",
+  storageBucket: "simple-firebase-authenti-ddc20.appspot.com",
+  messagingSenderId: "150777142367",
+  appId: "1:150777142367:web:adf9c470f0a36f4f45cfea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
